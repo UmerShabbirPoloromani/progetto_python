aa7f88774f087932d64c6b4549575bdcf8c80800
CirelliDavide06 <davideseiquattrozerosei@gmail.com>
2025-01-22 11:39:25 +0100

    fate il json

--- main.py
+++ main.py
@@ -1,30 +1,18 @@
-from flask import Flask, request, render_template
 from bs4 import BeautifulSoup  # Per modificare l'HTML
 import json, os, random
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template("paginaPrincipale.html")
+@app.route('/')
+def pagina_principale():
+    return render_template('paginaPrincipale.html')
 
-@app.route("/aggiungi_paragrafo", methods=["POST"])
-def aggiungi_paragrafo():
-    
-    # Modifica il file HTML
-    with open("templates/paginaPrincipale.html", "r", encoding="utf-8") as file:
-        soup = BeautifulSoup(file, "html.parser")
-    
-    # Aggiungi il nuovo paragrafo al body
-    nuovo_elemento = soup.new_tag("p")
-    nuovo_elemento.string = "nuovo_paragrafo"
-    soup.body.append(nuovo_elemento)
-    
-    # Salva le modifiche al file HTML
-    with open("templates/paginaPrincipale.html", "w", encoding="utf-8") as file:
-        file.write(str(soup))
-    
-    return "Paragrafo aggiunto con successo!"
+@app.route('/aggiorna', methods=['POST'])
+def aggiorna():
+    # Quando si clicca sul bottone, si reindirizza alla pagina aggiornata
+    nuovo_testo = "Il contenuto del paragrafo è stato aggiornato!"
+    return render_template('paginaAggiornata.html', paragrafo=nuovo_testo)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
