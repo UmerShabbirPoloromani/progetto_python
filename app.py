197a77d8cddaf70c9bda922fb6eb5d6c63f1e27d
CirelliDavide06 <davideseiquattrozerosei@gmail.com>
2025-01-22 12:20:52 +0100

    hyhhghgvvgvg

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, render_template
+from bs4 import BeautifulSoup  # Per modificare l'HTML
+import json, os, random
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+   
+    return render_template("paginaPrincipale.html")
+
+@app.route("/inserisci")
+def inserisci():
+    return "ciao"
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- main.py
+++
@@ -1,4 +0,0 @@
-from bs4 import BeautifulSoup  # Per modificare l'HTML
-import json, os, random
-from flask import Flask, render_template, request, redirect, url_for
-
