9dc4aec56c0449b7853d4c6b07b9ee365b8d27c9
CirelliDavide06 <davideseiquattrozerosei@gmail.com>
2025-01-22 10:32:21 +0100

    asd

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template
+from bs4 import BeautifulSoup  # Per modificare l'HTML
 
 app = Flask(__name__)
 
@@ -6,11 +7,23 @@ app = Flask(__name__)
 def index():
     return render_template("paginaPrincipale.html")
 
-@app.route("/process", methods=["POST"])
-def process():
-    # Recupera i dati inviati dal form
-    dato = request.form.get("dato")
-    return f"Hai inviato: {dato}"
+@app.route("/aggiungi_paragrafo", methods=["POST"])
+def aggiungi_paragrafo():
+    
+    # Modifica il file HTML
+    with open("templates/paginaPrincipale.html", "r", encoding="utf-8") as file:
+        soup = BeautifulSoup(file, "html.parser")
+    
+    # Aggiungi il nuovo paragrafo al body
+    nuovo_elemento = soup.new_tag("p")
+    nuovo_elemento.string = "nuovo_paragrafo"
+    soup.body.append(nuovo_elemento)
+    
+    # Salva le modifiche al file HTML
+    with open("templates/paginaPrincipale.html", "w", encoding="utf-8") as file:
+        file.write(str(soup))
+    
+    return "Paragrafo aggiunto con successo!"
 
 if __name__ == "__main__":
     app.run(debug=True)
